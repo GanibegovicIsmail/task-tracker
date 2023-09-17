857c8a01b8a7a1e5ba9d6d793e9aee34a7474614
Ismail Ganibegovic <ismail.ganibegovic.20@size.ba>
2023-09-17 15:19:55 +0200

    Feat: Final Changes Routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import Header from './components/Header'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import Footer from './components/Footer'
 import Tasks from './components/Tasks'
 import {useState, useEffect} from 'react' 
 import AddTask from './components/AddTask'
+import About from './components/About'
+
 
 const App = () => {
   const [showAddTask, setShowAddTask] = useState(false)
@@ -86,18 +90,37 @@ const deleteTask = async (id) =>{
 }
 
 
-  return (
-    <div className="container">
-      <Header onAdd={()=>setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
-      {showAddTask && <AddTask onAdd={addTask}/>}
-      <Tasks tasks={tasks} onDelete=
-      {deleteTask} onToggle={toggleReminder}/>
+return (
+  <Router>
+    <div className='container'>
+      <Header
+        onAdd={() => setShowAddTask(!showAddTask)}
+        showAdd={showAddTask}
+      />
+      <Routes>
+        <Route
+          path='/'
+          element={
+            <>
+              {showAddTask && <AddTask onAdd={addTask} />}
+              {tasks.length > 0 ? (
+                <Tasks
+                  tasks={tasks}
+                  onDelete={deleteTask}
+                  onToggle={toggleReminder}
+                />
+              ) : (
+                'No Tasks To Show'
+              )}
+            </>
+          }
+        />
+        <Route path='/about' element={<About />} />
+      </Routes>
+      <Footer />
     </div>
-  )
-}
-
-Header.defaultProps = {
-  title: 'Task Tracker'
+  </Router>
+)
 }
 
 export default App;
