288aff3ed68cdc675719a54881313e5765b8ebb6
Ismail Ganibegovic <ismail.ganibegovic.20@size.ba>
2023-09-17 12:20:58 +0200

    Feat: Server Addition

--- src/App.js
+++ src/App.js
@@ -27,10 +27,22 @@ const App = () => {
 
 
 //Add Task
-const addTask = (task) => {
-  const id = Math.floor(Math.random() * 10000) + 1
-  const newTask = { id, ...task}
-  setTasks([...tasks, newTask])
+const addTask = async (task) => {
+  const res = await fetch('http://localhost:5000/tasks', {
+    method: 'POST',
+    header: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(task),
+  })
+
+  const data = await res.json()
+
+  setTasks([...tasks, data])
+
+  // const id = Math.floor(Math.random() * 10000) + 1
+  // const newTask = { id, ...task}
+  // setTasks([...tasks, newTask])
 }
 
 
